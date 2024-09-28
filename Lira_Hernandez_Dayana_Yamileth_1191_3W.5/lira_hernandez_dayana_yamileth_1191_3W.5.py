61eb9b38e875688e82af260317234cef6d1d0725
Lira7682 <a.23308051281191@cbtis128.edu.mx>
2024-09-27 18:08:13 -0600

    Add files via upload

---
+++ Lira_Hernandez_Dayana_Yamileth_1191_3W.5/lira_hernandez_dayana_yamileth_1191_3W.5.py
@@ -0,0 +1,13 @@
+print (" ") #agregar espacio
+print ("Lira Hernandez Dayana Yamileth: determina si el valor ingresado esta entre los numero 1 y 12")
+print (" ")#agregar espacio
+
+numero = int(input("Ingresa un número natural: "))#instruccion para agregar valor
+
+# Verificar si el número está entre 1 y 12
+if 1 <= numero <= 12: #if indica si el valor esta entre o no del 1 y 12
+    print(f"El {numero} está entre 1 y 12") #imprime si esta o no entre 1 y 12
+else: #else indica si el valor esta entre o no del 1 y 12
+    print(f"El {numero} no esta entre 1 y 12") #imprime si esta o no entre 1 y 12
+    
+print (" ") #agregar espacio
